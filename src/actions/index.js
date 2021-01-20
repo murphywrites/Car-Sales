d765e6572d0ecaac99eae1aa224c0b8629e33276
Michael Murphy <mmurph917@michaels-mbp.lan>
2021-01-20 09:32:45 -0600

    Added Features working

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+export const ADD_FEATURE = "ADD_FEATURE"
+
+export const addFeature = feature => {
+    console.log("feature from addFeature: ", feature)
+    return {
+        type: ADD_FEATURE,
+        payload: feature
+    }
+}
